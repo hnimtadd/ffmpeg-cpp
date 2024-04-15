d54556b26472a4a51842ebc7820e73870f6cb09c
Dat Nguyen <minhdat15012002@gmail.com>
2024-04-15 10:12:05 +0700

    feat: stream scripts

--- scripts/stream.sh
+++ scripts/stream.sh
@@ -1,16 +1,19 @@
-RTSP_URL=/Users/hnimtadd/Project/Learn/cmake/learn-cpp/assets/sample.mp4
+RTSP_URL=/Users/hnimtadd/Project/Learn/cmake/learn-cpp/assets/encode_mp4.mp4
 PUBLISHING_URL=rtsp://171.244.62.138:9554/live/demo
 REMUX_URL=rtsp://171.244.62.138:9554/live/demo2
 $(which ffmpeg) \
-	-stream_loop -1 \
 	-re \
+	-stream_loop -1 \
 	-i $RTSP_URL \
-	-threads 4 \
-	-vcodec libx264 \
+	-threads 10 \
+	-force_key_frames "expr:gte(t,n_forced*5)" \
 	-rtsp_transport tcp \
 	-filter:v fps=10 \
-	-force_key_frames "expr:gte(t,n_forced*5)" \
 	-an \
 	-sn \
 	-f rtsp \
-	$PUBLISHING_URL
+	$PUBLISHING_URL &
+(sleep 3 && $(which ffplay) -i "$PUBLISHING_URL") &
+wait
+
+# replay the stream
